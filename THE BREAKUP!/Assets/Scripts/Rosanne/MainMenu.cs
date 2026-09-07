2a04085ab2af870895b0c89e115b6438d74c2bde
R04H <58626623+R04H@users.noreply.github.com>
2026-09-07 09:35:20 +0200

    music

--- THE BREAKUP!/Assets/Scripts/Rosanne/MainMenu.cs
+++ THE BREAKUP!/Assets/Scripts/Rosanne/MainMenu.cs
@@ -5,7 +5,11 @@ public class MainMenu : MonoBehaviour
 {
   public void PlayGame()
   {
-      SceneManager.LoadSceneAsync()
+        SceneManager.LoadSceneAsync(1);
   }
-    
+
+  public void QuitGame()
+  {
+        Application.Quit();
+  }
 }
